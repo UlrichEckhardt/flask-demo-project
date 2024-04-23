e31058e05f11f432e018db946ba578cfdbcdcf29
Ulrich Eckhardt <ulrich.eckhardt@base-42.de>
2024-04-23 21:01:35 +0200

    app.py: add some parametrization

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.get("/hello")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.get("/hello/<user>")
+def hello_world(user: str):
+    return f"<p>Hello, {user}!</p>"
 
 @app.get("/")
 def index():
-    return app.redirect("/hello")
+    return app.redirect("/hello/world")
