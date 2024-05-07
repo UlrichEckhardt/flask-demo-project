37468e78cfcacf37c41bc2013aa61407643d5cff
Ulrich Eckhardt <ulrich.eckhardt@base-42.de>
2024-05-07 08:33:14 +0200

    app/routes.py: Add missing docstrings and remove unused import

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,4 @@
+"""routing table"""
 from datetime import datetime
 from flask import render_template
 from . import app
@@ -5,6 +6,7 @@ from . import app
 
 @app.get("/hello/<user>")
 def hello_world(user: str):
+    """render a greeting in HTML"""
     return render_template(
         "hello_world.html",
         name=user,
@@ -14,9 +16,11 @@ def hello_world(user: str):
 
 @app.get("/")
 def index():
+    """default endpoint that redirects to the greeter"""
     return app.redirect("/hello/world")
 
 
 @app.get("/favicon.ico")
 def favicon():
+    """favicon"""
     return app.send_static_file("icons/favicon.ico")
