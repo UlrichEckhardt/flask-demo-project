141b162cb76df456386ada86885d847f27002ee2
Ulrich Eckhardt <ulrich.eckhardt@base-42.de>
2024-05-07 08:31:10 +0200

    app/entrypoint.py: Disable pylint message
    The entrypoint is supposed to import modules in order to cause side
    effects from their import. The disabled warning was that the imported
    modules are unused.

--- app/entrypoint.py
+++ app/entrypoint.py
@@ -1,4 +1,5 @@
 """Entry point for the Flask application."""
 
+# pylint:disable=unused-import
 from . import app  # For application discovery by the 'flask' command.
 from . import routes  # Routes
